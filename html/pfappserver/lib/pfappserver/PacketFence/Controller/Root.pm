08ef31d4cddd6fbf30408cbb4b0249d2fd764d19
Cecilia Tran <ctran@ctran-mac-4.local>
2018-06-26 23:53:01 -0700

    changes to license enforcement

--- html/pfappserver/lib/pfappserver/PacketFence/Controller/Root.pm
+++ html/pfappserver/lib/pfappserver/PacketFence/Controller/Root.pm
@@ -18,7 +18,7 @@ use warnings;
 use Moose;
 use namespace::autoclean;
 use pf::db;
-use pf::a3_entitlement qw(is_usage_under_capacity is_entitlement_expired);
+use pf::a3_entitlement qw(get_current_moving_avg is_usage_under_capacity is_entitlement_expired);
 use pf::config qw(%Config);
 use pf::util;
 use pf::log;
@@ -46,9 +46,14 @@ auto
 
 sub auto :Private {
     my ( $self, $c ) = @_;
+    my $logger = get_logger();
     $c->stash->{readonly_mode} = db_check_readonly();
+    my ($status, $curr_mov_avg) = pf::a3_entitlement::get_current_moving_avg();
+    $c->stash->{current_mov_avg} = $curr_mov_avg;
     $c->stash->{is_usage_under_capacity} = pf::a3_entitlement::is_usage_under_capacity();
     $c->stash->{is_entitlement_expired} = pf::a3_entitlement::is_entitlement_expired();
+    $logger->info($c->stash->{current_mov_avg});
+
     return 1;
 }
 
