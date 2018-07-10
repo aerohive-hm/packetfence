7f0804a307822893c63cb6fef5fe2f7c320595ae
Cecilia Tran <ctran@ctran-mac-4.local>
2018-07-09 23:38:43 -0700

    updated how file upload is added

--- html/pfappserver/lib/pfappserver/PacketFence/Controller/Config/PKI_Provider.pm
+++ html/pfappserver/lib/pfappserver/PacketFence/Controller/Config/PKI_Provider.pm
@@ -84,13 +84,15 @@ before [qw(clone view _processCreatePost update)] => sub {
 };
 
 sub create_type : Path('create') : Args(1) {
-    my ($self, $c, $type) = @_;
-    my $logger = get_logger();
+    my ($self, $c, $type, $assignments) = @_;
+    # my $logger = get_logger();
     my $model = $self->getModel($c);
     my $itemKey = $model->itemKey;
     $c->stash->{$itemKey}{type} = $type;
-    $logger->info("model: " .Dumper($c));
+    # $logger->info("assignments: " .Dumper($assignments));
+    # $logger->info("assignments: $assignments");
     $c->forward('create');
+
 }
 
 sub processCertificate :Path('processCertificate') :Args(1) {
