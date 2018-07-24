0dc4f0e0b15303a3a380da9375952d7d9bc894fa
Weiyu (Thomas) Wang <wywang@aerohive.com>
2018-07-24 12:54:53 +0800

    for dev and release logic

--- addons/packages/a3-rpm-build-expect.sh
+++ addons/packages/a3-rpm-build-expect.sh
@@ -6,7 +6,11 @@ set DATE [lindex $argv 2]
 set BUILD_DIR [lindex $argv 3]
 set PASSPHRASE [lindex $argv 4]
 #spawn ./sign_image.sh $signfile  testprivatekey.pem $outfile
-spawn rpmbuild -ba --sign --define "ver $VERSION" --define "snapshot 1" --define "dist $DIST" --define "rev 0.$DATE" $BUILD_DIR/SPECS/A3.spec 
+if [ "$BUILD_TYPE" != "RELEASE" ]; then
+  spawn rpmbuild -ba --sign --define "ver $VERSION" --define "snapshot 1" --define "dist $DIST" --define "rev 0.$DATE" $BUILD_DIR/SPECS/A3.spec 
+else 
+  spawn rpmbuild -ba --sign --define "ver $VERSION" --define "dist $DIST" --define "rev 0.$DATE" $BUILD_DIR/SPECS/A3.spec 
+fi  
 expect {
 "Enter pass phrase:" {send "$PASSPHRASE\r"}
 }
