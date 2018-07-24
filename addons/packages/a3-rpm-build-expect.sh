b709a5e18071374298a1cec18276e0ad741ac534
Weiyu (Thomas) Wang <wywang@aerohive.com>
2018-07-24 13:50:16 +0800

    fix syntax error

--- addons/packages/a3-rpm-build-expect.sh
+++ addons/packages/a3-rpm-build-expect.sh
@@ -7,11 +7,12 @@ set BUILD_DIR [lindex $argv 3]
 set PASSPHRASE [lindex $argv 4]
 set BUILD_TYPE $::env(BUILD_TYPE)
 #spawn ./sign_image.sh $signfile  testprivatekey.pem $outfile
-if [ "$BUILD_TYPE" != "RELEASE" ]; then
+if {$BUILD_TYPE" == "RELEASE"} {
   spawn rpmbuild -ba --sign --define "ver $VERSION" --define "snapshot 1" --define "dist $DIST" --define "rev 0.$DATE" $BUILD_DIR/SPECS/A3.spec 
-else 
+}
+else { 
   spawn rpmbuild -ba --sign --define "ver $VERSION" --define "dist $DIST" --define "rev 0.$DATE" $BUILD_DIR/SPECS/A3.spec 
-fi  
+}
 expect {
 "Enter pass phrase:" {send "$PASSPHRASE\r"}
 }
