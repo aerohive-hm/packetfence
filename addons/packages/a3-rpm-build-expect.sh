4ee63e0404396d0b3bcdaf1ae5da792c56e429d0
Weiyu (Thomas) Wang <wywang@aerohive.com>
2018-07-24 13:57:45 +0800

    fix indentation

--- addons/packages/a3-rpm-build-expect.sh
+++ addons/packages/a3-rpm-build-expect.sh
@@ -10,8 +10,7 @@ puts $BUILD_TYPE
 #spawn ./sign_image.sh $signfile  testprivatekey.pem $outfile
 if {$BUILD_TYPE eq "RELEASE"} {
   spawn rpmbuild -ba --sign --define "ver $VERSION" --define "snapshot 1" --define "dist $DIST" --define "rev 0.$DATE" $BUILD_DIR/SPECS/A3.spec 
-}
-else { 
+} else {
   spawn rpmbuild -ba --sign --define "ver $VERSION" --define "dist $DIST" --define "rev 0.$DATE" $BUILD_DIR/SPECS/A3.spec 
 }
 expect {
