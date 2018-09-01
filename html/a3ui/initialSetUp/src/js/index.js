5e22cff99796df395841364df0ee0f3abe44b684
jiuxiang meng <jxmeng@aerohive.com>
2018-09-01 15:27:35 +0800

    aerohiveCloudCtl style done

--- html/a3ui/initialSetUp/src/js/index.js
+++ html/a3ui/initialSetUp/src/js/index.js
@@ -32,7 +32,7 @@ class App extends Component {
         this.state = {
             i18n:{},
             loading : false,
-            show:"adminUser",
+            show:"aerohiveCloud",
             // getStart
             // adminUser
             // networks
