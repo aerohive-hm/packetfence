a83986968f02995e4d90ba0f652a1e87bf6eb1ba
jiuxiang meng <jxmeng@aerohive.com>
2018-09-01 15:22:41 +0800

    adminUserCtl style done

--- html/a3ui/initialSetUp/src/js/index.js
+++ html/a3ui/initialSetUp/src/js/index.js
@@ -32,7 +32,7 @@ class App extends Component {
         this.state = {
             i18n:{},
             loading : false,
-            show:"joinCluster",
+            show:"adminUser",
             // getStart
             // adminUser
             // networks
