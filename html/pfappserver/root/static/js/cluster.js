876317c6c29b0369be135c5d2999a52012f2488f
Cecilia Tran <ctran@ctran-mac-4.local>
2018-09-04 15:51:17 -0700

    updated cluster and cloud javascript functions

--- html/pfappserver/root/static/js/cluster.js
+++ html/pfappserver/root/static/js/cluster.js
@@ -12,26 +12,25 @@ function getClusterStatusInfo(){
 
 function submitClusterNode(){
   var base_url = window.location.origin;
-  // $.ajax({
-  //     type: 'POST',
-  //     url: base_url + '/a3/api/v1/configuration/cluster',
-  //     data: ,
-  //     dataType: 'json',
-  //     processData: false,
-  //     contentType: false,
-  //     success: function(data){
-  //
-  //     },
-  //     error: function(data){
-  //       alert("something went wrong");
-  //       var errMsg = data.status_msg;
-  //       if (errMsg != null ) {
-  //           document.getElementById('errorMessage').innerHTML = errMsg;
-  //           $("#success-alert").show();
-  //           setTimeout(function (){
-  //             $("#success-alert").slideUp(500);
-  //           }, 3000);
-  //       }
-  //     }
-  // });
+  $.ajax({
+      type: 'POST',
+      url: base_url + '/a3/api/v1/configuration/cluster',
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+      success: function(data){
+
+      },
+      error: function(data){
+        alert("something went wrong");
+        var errMsg = data.status_msg;
+        if (errMsg != null ) {
+            document.getElementById('errorMessage').innerHTML = errMsg;
+            $("#success-alert").show();
+            setTimeout(function (){
+              $("#success-alert").slideUp(500);
+            }, 3000);
+        }
+      }
+  });
 }
