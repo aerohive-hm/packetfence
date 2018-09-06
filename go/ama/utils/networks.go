83107a1a85fa572ca5dc2ef9a6b931158532f902
juanli <juanli@aerohive.com>
2018-09-06 11:02:17 +0800

    Adding APIs to get the local management IP

--- go/ama/utils/networks.go
+++ go/ama/utils/networks.go
@@ -28,5 +28,15 @@ func GetA3DefaultGW() string {
 	return ""
 }
 
-
+func GetIpAddr(ifname string) string {
+	managementIface, errint := GetIfaceList(ifname)
+	if errint < 0 {
+		fmt.Errorf("Get interfaces infomation failed")
+		return ""
+	}
+	for _, iface := range managementIface {
+		return iface.IpAddr
+	}
+	return ""
+}
 
