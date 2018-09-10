9ccaa9e9b2e5002eb51ab3dd01e51558e5064d89
jma <jma@aerohive.com>
2018-09-10 16:58:21 -0700

    Clean up and add tmp certs directory under /usr/local/pf/conf/ssl/tmp_certs

--- html/pfappserver/lib/pfappserver/PacketFence/Controller/CertificateUpload.pm
+++ html/pfappserver/lib/pfappserver/PacketFence/Controller/CertificateUpload.pm
@@ -61,17 +61,11 @@ sub uploadKey :Chained('/') :PathPart('uploadKey') :Args(0) :AdminRole('CERTIFIC
 
     my $logger = get_logger();
 
-    $logger->info("jma_debug in uploadKey");
-    # $logger->info("\ntype: $type");
-
     if ($c->request->method eq 'POST'){
         my $filename  = $c->request->{uploads}->{file}->{filename};
-        my $name      = $c->request->{query_parameters}->{name};
-        my $qualifier = $c->request->{query_parameters}->{qualifier};
         my $source    = $c->request->{uploads}->{file}->{tempname};
-        my $targetdir = '/usr/local/pf/conf/ssl';
+        my $targetdir = '/usr/local/pf/conf/ssl/tmp_certs';
         my $template  = 'cert-XXXXXX';
-        my $tempfile  = "/usr/local/pf/conf/ssl/tls_certs/cert-XXXXXX.tmp";
         my $filesize  = -s $source;
 
         $logger->info("filename: $filename, size: $filesize, location: $source");
@@ -90,7 +84,6 @@ sub uploadKey :Chained('/') :PathPart('uploadKey') :Args(0) :AdminRole('CERTIFIC
             $c->response->status($STATUS::BAD_REQUEST);
 
             $c->stash->{status_msg} = $c->loc("File is invalid. Try again.");
-            $logger->info("jma_debug c: " .Dumper($c));
             return;
         }
 
@@ -101,7 +94,7 @@ sub uploadKey :Chained('/') :PathPart('uploadKey') :Args(0) :AdminRole('CERTIFIC
             unlink($tmp_filename);
 
             $c->response->status($STATUS::INTERNAL_SERVER_ERROR);
-            $c->stash->{status_msg} = $c->loc("Unable to install certificate key. Try again.");
+            $c->stash->{status_msg} = $c->loc("Unable to install certificate. Try again.");
             return;
         }
         #
@@ -138,12 +131,10 @@ sub uploadServerCert :Chained('/') :PathPart('uploadServerCert') :Args(0) :Admin
 
     my $logger = get_logger();
 
-    $logger->info("jma_debug inside uploadServerCert");
-
     if ($c->request->method eq 'POST'){
         my $filename  = $c->request->{uploads}->{file}->{filename};
         my $source    = $c->request->{uploads}->{file}->{tempname};
-        my $targetdir = '/usr/local/pf/conf/ssl';
+        my $targetdir = '/usr/local/pf/conf/ssl/tmp_certs';
         my $template  = 'cert-XXXXXX';
         my $filesize  = -s $source;
 
@@ -172,7 +163,7 @@ sub uploadServerCert :Chained('/') :PathPart('uploadServerCert') :Args(0) :Admin
             unlink($tmp_filename);
 
             $c->response->status($STATUS::INTERNAL_SERVER_ERROR);
-            $c->stash->{status_msg} = $c->loc("Unable to install certificate key. Try again.");
+            $c->stash->{status_msg} = $c->loc("Unable to install certificate. Try again.");
             return;
         }
 
@@ -197,12 +188,10 @@ sub uploadCACert :Chained('/') :PathPart('uploadCACert') :Args(0) :AdminRole('CE
 
     my $logger = get_logger();
 
-    $logger->info("jma_debug inside uploadCACert");
-
     if ($c->request->method eq 'POST'){
         my $filename  = $c->request->{uploads}->{file}->{filename};
         my $source    = $c->request->{uploads}->{file}->{tempname};
-        my $targetdir = '/usr/local/pf/conf/ssl';
+        my $targetdir = '/usr/local/pf/conf/ssl/tmp_certs';
         my $template  = 'cert-XXXXXX';
         my $filesize  = -s $source;
 
@@ -231,7 +220,7 @@ sub uploadCACert :Chained('/') :PathPart('uploadCACert') :Args(0) :AdminRole('CE
             unlink($tmp_filename);
 
             $c->response->status($STATUS::INTERNAL_SERVER_ERROR);
-            $c->stash->{status_msg} = $c->loc("Unable to install CA certificate. Try again.");
+            $c->stash->{status_msg} = $c->loc("Unable to install certificate. Try again.");
             return;
         }
 
@@ -261,21 +250,21 @@ sub verifyCert :Chained('/') :PathPart('verifyCert') :Args(0) :AdminRole('CERTIF
     my ($self, $c) = @_;
 
     my $logger = get_logger();
-    $logger->info("jma_debug inside verifyCert");
+
 
     if ($c->request->method eq 'POST') {
         my $qualifier = $c->request->{query_parameters}->{qualifier};
         my $key_path = $c->request->{query_parameters}->{key_path};
         my $cert_path = $c->request->{query_parameters}->{cert_path};
         my $cn_server;
-        $logger->info("jma_debug key_path $key_path cert_path $cert_path qualifier $qualifier");
+
         my $key_md5 = `openssl rsa -noout -modulus -in $key_path| openssl md5`;
         my $cert_md5 = `openssl x509 -noout -modulus -in $cert_path| openssl md5`;
-        $logger->info("jma_debug key_md5 $key_md5 cert_md5 $cert_md5");
+
         if ($key_md5 eq $cert_md5) {
             #eap-tls certs will be put in raddb/certs
             if ($qualifier eq "eap") {
-                $logger->info("jma_debug inside eap verify");
+                $logger->info("Verified $key_path against $cert_path");
                 if ( ! rename($key_path, $radius_server_key) ) {
                     $logger->warn("Failed to move certificate file $key_path into place at $radius_server_key: $!");
                     $c->response->status($STATUS::INTERNAL_SERVER_ERROR);
@@ -291,7 +280,6 @@ sub verifyCert :Chained('/') :PathPart('verifyCert') :Args(0) :AdminRole('CERTIF
 
                 $c->stash->{CN_Server} = pf::util::get_cert_subject_cn($radius_server_cert);
             } elsif ($qualifier eq "https") {
-                $logger->info("jma_debug inside https verify");
                 #https certs will be put in conf/ssl
                 if ( ! rename($key_path, $server_key) ) {
                     $logger->warn("Failed to move certificate file $key_path into place at $server_key: $!");
@@ -307,7 +295,6 @@ sub verifyCert :Chained('/') :PathPart('verifyCert') :Args(0) :AdminRole('CERTIF
                 }
 
                 $cn_server = pf::util::get_cert_subject_cn($server_cert);
-                $logger->info("jma_debug CN_server $cn_server");
                 system("cat $server_cert $server_key > $server_pem");
                 $c->stash->{CN_Server} = pf::util::get_cert_subject_cn($server_cert);
             }
@@ -320,7 +307,7 @@ sub verifyCert :Chained('/') :PathPart('verifyCert') :Args(0) :AdminRole('CERTIF
             if(unlink($cert_path) != 1) {
                 $logger->warn("Failed to remove $cert_path $!");
             }
-            $c->stash->{status_msg} = $c->loc("Unable to verify certificate. Try again.");
+            $c->stash->{status_msg} = $c->loc("Unable to verify the certificate. Try again.");
             return;
         }
     } else {
@@ -339,7 +326,6 @@ Usage: /readCert
 sub readCert :Chained('/') :PathPart('readCert') :Args(0) :AdminRole('CERTIFICATE_UPDATE') {
     my ($self, $c) = @_;
     my $logger = get_logger();
-    $logger->info("jma_debug inside readCert");
 
     if ($c->request->method eq 'GET') {
         my $qualifier = $c->request->{query_parameters}->{qualifier};
@@ -370,7 +356,6 @@ sub downloadCert :Chained('/') :PathPart('downloadCert') :Args(0) :AdminRole('CE
     my $cert_path;
     my $cert;
     my $in;
-    $logger->info("jma_debug inside readCert");
 
     if ($c->request->method eq 'GET') {
         my $qualifier = $c->request->{query_parameters}->{qualifier};
@@ -409,8 +394,6 @@ Usage: /removeCert
 sub removeCert :Chained('/') :PathPart('removeCert') :Args(0) :AdminRole('CERTIFICATE_UPDATE') {
     my ($self, $c) = @_;
     my $logger = get_logger();
-
-    $logger->info("jma_debug inside removeCert");
 
     if ($c->request->method eq 'POST') {
         my $cert_path = $c->request->{query_parameters}->{file_path};
