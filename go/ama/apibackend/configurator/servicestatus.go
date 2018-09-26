fa6968777fec001fa0a57fcb8402878f05706711
czhong <czhong@aerohive.com>
2018-09-26 16:56:36 +0800

    A3-690   fix an issue of iptable can't start after reboot.

--- go/ama/apibackend/configurator/servicestatus.go
+++ go/ama/apibackend/configurator/servicestatus.go
@@ -38,6 +38,7 @@ func handleGetServiceStatus(r *http.Request, d crud.HandlerData) []byte {
 
 	if msg == "100" {
 		utils.UpdateCurrentlyAt()
+		utils.ExecShell(`pfcmd service iptables start`)
 	}
 
 	return []byte(fmt.Sprintf(`{"code":"%s", "percentage":"%s"}`, code, msg))
