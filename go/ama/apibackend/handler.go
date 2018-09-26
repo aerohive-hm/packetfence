bd2d101fa6970df1954e7edd6da347defbaa0187
czhong <czhong@aerohive.com>
2018-09-26 19:50:50 +0800

    A3-690   fix an issue with service start process can't reach 100%.

--- go/ama/apibackend/handler.go
+++ go/ama/apibackend/handler.go
@@ -70,7 +70,7 @@ func Handle(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	if d.Cmd == "configurator" &&
 		utils.IsFileExist(utils.A3CurrentlyAt) &&
 		(d.SubCmd != "networks" &&
-			d.SubCmd != "servicestatus") {
+			d.SubCmd != "servicesstatus") {
 		w.WriteHeader(http.StatusForbidden)
 		return
 	}
