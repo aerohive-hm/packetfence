660c772be545f4d41a5b8738c0c2b6a4dc584a92
juanli <juanli@aerohive.com>
2018-10-17 17:31:46 +0800

    Fix issue A3-751: Reporting, username is not correct for macauth+portal

--- lib/pf/locationlog.pm
+++ lib/pf/locationlog.pm
@@ -356,23 +356,6 @@ sub locationlog_synchronize {
 
     $voip_status = $NO_VOIP if !defined $voip_status || $voip_status ne $VOIP; #Set the default voip status
     my $logger = get_logger();
-
-
-    my ($seconds, $microseconds) = Time::HiRes::gettimeofday();
-    my $timestamp = $seconds * 1000 * 1000 + $microseconds;
-    my $url = "http://127.0.0.1:10000/api/v1/event/report";
-    my %ama_data = (
-        ah_tablename => "radacct", 
-        ah_timestamp => "$timestamp",
-        username => $user_name,
-        acctstatustype => "Interim-Update-Username",
-        callingstationid => $mac,
-        acctupdatetime => "$seconds",
-    );
-
-
-    pf::util::call_url("POST", $url, \%ama_data);
-
     $logger->trace(sub {"sync locationlog with ifDesc " . ($ifDesc // "undef")});
     $logger->trace("locationlog_synchronize called");
 
