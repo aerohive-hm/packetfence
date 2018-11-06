ff0920b2573d3c3b5d1681e93ec7392b6bf61aa5
Weiyu (Thomas) Wang <wywang@aerohive.com>
2018-11-06 12:08:47 +0800

    A3-889 create post process script after pf service started during update

--- a3_update/a3_cluster_update.pl
+++ a3_update/a3_cluster_update.pl
@@ -372,6 +372,22 @@ for my $node_ip (@all_nodes_ip) {
 
 restart_pf_all_services();
 
+#call this after pf started
+for my $node_ip (@all_nodes_ip) {
+    $ret = pf::a3_cluster_update::remote_api_call_post($node_ip, 'a3/post_process', {'opts'=>['before']});
+    if ($ret != 0) {
+      commit_cluster_update_log("Post process before on node $node_ip, please check detail log on peer node");
+      commit_progress_log("error at $node_ip");
+    }
+}
+for my $node_ip (@all_nodes_ip) {
+    $ret = pf::a3_cluster_update::remote_api_call_post($node_ip, 'a3/post_process', {'opts'=>['after']});
+    if ($ret != 0) {
+      commit_cluster_update_log("Post process after on node $node_ip, please check detail log on peer node");
+      commit_progress_log("error at $node_ip");
+    }
+}
+
 #Finish update
 print "\nThe whole update process finished, please resume the pf service!!\n";
 commit_cluster_update_log("The whole update process finished, please resume the pf service!!");
