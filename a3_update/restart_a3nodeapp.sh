bb3ec5d3fd057c74b00801c2c7530e251b33acfb
Weiyu (Thomas) Wang <wywang@aerohive.com>
2018-11-07 22:22:25 +0800

    add restart a3nodeapp systemd job and make it restarted in post_process step

---
+++ a3_update/restart_a3nodeapp.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#should be good enough for now
+sleep 1800
+systemctl restart a3-nodeapp
--- a3_update/restart_nodeapp_wrapper.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var spawn = require('child_process').spawn;
-
-spawn('/usr/local/pf/a3_update/restart_nodeapp_wrapper.sh')
-process.exit(0)
