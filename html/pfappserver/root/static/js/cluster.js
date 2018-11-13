b4481cb7429cfad2e8cf9c05bbd30a2e924e21cc
Cecilia Tran <ctran@vtseng-t430.aerohive.com>
2018-11-12 16:16:16 -0800

    added sorting, refresh only on link status, prevented 10 sec refresh on vrid and shared key

--- html/pfappserver/root/static/js/cluster.js
+++ html/pfappserver/root/static/js/cluster.js
@@ -1,3 +1,55 @@
+var sampleData = {
+    "sharedkey":"aerohive",
+    "vrid":"100",
+// the following is GET only
+    "interfaces":[
+        {
+            "name":"eth0",
+            "vip":"10.0.123.254"
+        },
+        {
+            "name":"eth0.10",
+            "vip":"192.168.10.254"
+        },
+        {
+            "name":"eth1",
+            "vip":"10.0.234.254"
+        }
+    ],
+    "nodes": [
+        {
+            "hostname":"a3_node1.aerohive.com",
+            "ipaddr":"10.155.100.1",
+            "type":"master",
+            "status":"active"
+        },
+        {
+            "hostname":"a3_node9.aerohive.com",
+            "ipaddr":"10.155.100.2",
+            "type":"slave",
+            "status":"active"
+        },
+        {
+            "hostname":"a3_node3.aerohive.com",
+            "ipaddr":"10.155.100.3",
+            "type":"slave",
+            "status":"active"
+        },
+        {
+            "hostname":"a3_node5.aerohive.com",
+            "ipaddr":"10.155.100.3",
+            "type":"slave",
+            "status":"inactive"
+        },
+        {
+            "hostname":"a3_node2.aerohive.com",
+            "ipaddr":"10.155.100.3",
+            "type":"slave",
+            "status":"inactive"
+        }
+    ]
+};
+
 
 $(document).ready(function(){
     //functions in this file:
@@ -6,6 +58,8 @@ $(document).ready(function(){
     //submitClusterInfo()
     //removeClusterNode(nodeArray)
 
+    // saveCheckboxStatus();
+    // returnStoredCheckboxStatus();
     //to change submit info
     document.getElementById("submitNewClusterInfo").onclick = function(e){
         e.preventDefault();
@@ -44,6 +98,7 @@ $(document).ready(function(){
     $("#cluster-management-table-tbody tr").remove();
     $("#net-interfaces-table-tbody tr").remove();
     getClusterStatusInfo();
+    updateSharedIdVrid();
 
     //button press on trashcan, array, removeClusterNode(), removeClusterNode(nodeArray)
     document.getElementById('remove-node').onclick = function(e){
@@ -71,11 +126,12 @@ $(document).ready(function(){
     //reseting the form to original values from file
     document.getElementById('reset-form').onclick = function(e){
         e.preventDefault();
-        getClusterStatusInfo();
+        // getClusterStatusInfo();
+        updateSharedIdVrid();
     }
 
     //will update the table every 10 seconds to get the current status of node connection
-    setInterval("getClusterStatusInfo()", 10000);
+    setInterval("updateLinkStatusCluster()", 10000);
 });
 
 
@@ -126,7 +182,7 @@ function submitClusterInfo(){
                 setTimeout(function(){
                     $("#success-alert").slideUp(500);
                 }, 3000);
-                getClusterStatusInfo();
+                updateSharedIdVrid();
             }
         },
         error: function(data){
@@ -181,7 +237,31 @@ function removeClusterNode(nodeArray){
     });
 }
 
-//function to get cluster table data
+//this will update the value of the sharedKey and vrid only
+function updateSharedIdVrid(){
+  var base_url = window.location.origin;
+  $.ajax({
+      type: 'GET',
+      url: base_url + '/ama/cluster',
+      success: function(data){
+          data = jQuery.parseJSON(data.A3_data);
+          var vridvalue = data.vrid;
+          $("#vrid").val(vridvalue);
+          var sharedkeyvalue = data.sharedkey;
+          $("#sharedkey").val(sharedkeyvalue);
+      },
+      error: function(data){
+          document.getElementById('errorMessage').innerHTML = "Could not grab the Shared key and Virtual router ID info";
+          $("#success-alert").show();
+          setTimeout(function(){
+              $("#success-alert").slideUp(500);
+          }, 3000);
+      }
+    });
+
+}
+
+//function update and create the cluster tables only
 function getClusterStatusInfo(){
     var base_url = window.location.origin;
     $.ajax({
@@ -189,21 +269,19 @@ function getClusterStatusInfo(){
         url: base_url + '/ama/cluster',
         success: function(data){
             data = jQuery.parseJSON(data.A3_data);
-            var vridvalue = data.vrid; $("#vrid").val(vridvalue);
-            var sharedkeyvalue = data.sharedkey; $("#sharedkey").val(sharedkeyvalue);
-
+            sampleData.nodes = sortJSON(sampleData.nodes, 'hostname');
             $("#cluster-management-table-tbody tr").remove();
             $("#net-interfaces-table-tbody tr").remove();
-            //cluster management table
-            $.each(data.nodes, function(i, members){
-                if (members.type === "Master"){
-                    $("#cluster-management-table-tbody").prepend("<tr><td>" + "" + "</td><td>" + members.hostname + "</td><td>" + members.ipaddr + "</td><td>" +  members.type + "</td><td>" +  members.status + "</td></tr>");
-                } else {
 
-                    // if (members.status == Inactive)
-                    $("#cluster-management-table-tbody").append("<tr><td>" + "<input id='delete-cluster-node' type='checkbox' value='"+ members.hostname +"'/>" + "</td><td>" + members.hostname + "</td><td>" + members.ipaddr + "</td><td>" +  members.type + "</td><td>" +  members.status + "</td></tr>");
+            //cluster management table
+            $.each(sampleData.nodes, function(i, members){
+                if (members.type === "master"){
+                    $("#cluster-management-table-tbody").prepend("<tr><td>" + "" + "</td><td>" + members.hostname + "</td><td>" + members.ipaddr + "</td><td>" +  members.type + "</td><td id='linkStatus'>" +  members.status + "</td></tr>");
+                } else {
+                    $("#cluster-management-table-tbody").append("<tr><td>" + "<input id='delete-cluster-node' name='cluster-node-select' type='checkbox' value='"+ members.hostname +"'/>" + "</td><td>" + members.hostname + "</td><td>" + members.ipaddr + "</td><td>" +  members.type + "</td><td id='linkStatus-" + i + "'>" +  members.status + "</td></tr>");
                 }
             });
+
             //interfaces table
             $.each(data.interfaces, function(ethr, vip) {
                 if(ethr.indexOf('.') !== -1){ //if there is period in eth0
@@ -227,5 +305,32 @@ function getClusterStatusInfo(){
                 $("#success-alert").slideUp(500);
             }, 3000);
         }
+    });
+}
+
+//updates only the linkstatus column for each node
+function updateLinkStatusCluster(){
+  var base_url = window.location.origin;
+  $.ajax({
+      type: 'GET',
+      url: base_url + '/ama/cluster',
+      success: function(data){
+          data = jQuery.parseJSON(data.A3_data);
+          $.each(sampleData.nodes, function(i, members){
+              if (members.type === "master"){ $('#linkStatus').text(members.status); /*update master node*/ }
+              else { $('#linkStatus-' + i).text(members.status); /*update child cluster nodes*/ }
+          });
+      },error: function(data){
+        console.log("Was not able to update status");
+      }
+    });
+}
+
+//sort json data coming back for child nodes
+function sortJSON(data, key) {
+    return data.sort(function (a, b) {
+        var x = a[key];
+        var y = b[key];
+        if (x < y){ return -1 } else if (x > y){ return 1; } else { return 0; }
     });
 }
